20590d19e42d38a9ba23478aafcaef40cb0dcd48
ibtesam5d <ibtesam.hossain@gmail.com>
2023-04-03 19:02:16 -0600

    Home Page test

--- src/pages/home/Home.test.jsx
+++ src/pages/home/Home.test.jsx
@@ -3,6 +3,7 @@ import { expect, it } from "vitest";
 import { MemoryRouter } from "react-router-dom";
 import Home from "./Home";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Abook } from "../../../public/images/abook.svg";
 
 it("should show slider component", () => {
   const queryClient = new QueryClient();
@@ -16,4 +17,33 @@ it("should show slider component", () => {
   );
   const slide = screen.getByTestId("slide");
   expect(slide).toBeVisible();
+});
+
+it("should have src and alt attribute", () => {
+  const queryClient = new QueryClient();
+  const route = "/";
+  render(
+    <QueryClientProvider client={queryClient}>
+      <MemoryRouter initialEntries={[route]}>
+        <Home />
+      </MemoryRouter>
+    </QueryClientProvider>
+  );
+  const images = screen.getAllByRole("img");
+  images.map((img) => expect(img.src).toBeDefined());
+  images.map((img) => expect(img.alt).toBeDefined());
+});
+
+it("should show welcome image", () => {
+  const queryClient = new QueryClient();
+  const route = "/";
+  render(
+    <QueryClientProvider client={queryClient}>
+      <MemoryRouter initialEntries={[route]}>
+        <Home />
+      </MemoryRouter>
+    </QueryClientProvider>
+  );
+  const images = screen.getAllByRole("img");
+  expect(images[0].src).toEqual("http://localhost:3000/images/abook.svg");
 });
